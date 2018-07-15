73050a61b077367191f681c06c77b5a41b50166b
missmeow71 <sensuousdesigner@gmail.com>
2018-07-15 11:28:54 -0600

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres:///drill-3'
+  },
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL'
+  }
+};
